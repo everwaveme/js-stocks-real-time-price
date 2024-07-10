57e83cf4d4b80a5f3f6746b1a95f57aafdee2258
Farhad Sadykov <f.sadykov@yahoo.com>
2024-07-10 12:55:17 +0300

    deleted NaN condition

--- main.js
+++ main.js
@@ -13,35 +13,35 @@ async function getCoreData() {
     const vooQuoteAPI = 'https://finnhub.io/api/v1/quote?symbol=VOO&token=cq21q99r01ql95ncg0tgcq21q99r01ql95ncg0u0';
 
     //Requests
-    const aaplPrice = await fetch(aaplQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const aaplPercent = await fetch(aaplQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const aaplPrice = await fetch(aaplQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const aaplPercent = await fetch(aaplQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const cpPrice = await fetch(cpQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const cpPercent = await fetch(cpQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const cpPrice = await fetch(cpQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const cpPercent = await fetch(cpQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const dePrice = await fetch(deQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const dePercent = await fetch(deQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const dePrice = await fetch(deQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const dePercent = await fetch(deQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const googlPrice = await fetch(googlQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const googlPercent = await fetch(googlQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const googlPrice = await fetch(googlQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const googlPercent = await fetch(googlQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const hdPrice = await fetch(hdQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const hdPercent = await fetch(hdQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const hdPrice = await fetch(hdQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const hdPercent = await fetch(hdQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const msftPrice = await fetch(msftQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const msftPercent = await fetch(msftQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const msftPrice = await fetch(msftQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const msftPercent = await fetch(msftQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const neePrice = await fetch(neeQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const neePercent = await fetch(neeQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const neePrice = await fetch(neeQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const neePercent = await fetch(neeQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const tscoPrice = await fetch(tscoQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const tscoPercent = await fetch(tscoQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const tscoPrice = await fetch(tscoQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const tscoPercent = await fetch(tscoQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const txnPrice = await fetch(txnQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const txnPercent = await fetch(txnQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const txnPrice = await fetch(txnQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const txnPercent = await fetch(txnQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
-    const vooPrice = await fetch(vooQuoteAPI).then(res => res.json()).then(data => parseFloat(data.c).toFixed(2));
-    const vooPercent = await fetch(vooQuoteAPI).then(res => res.json()).then(data => parseFloat(data.dp).toFixed(2));
+    const vooPrice = await fetch(vooQuoteAPI).then(res => res.json()).then(data => Number(data.c.toFixed(2)));
+    const vooPercent = await fetch(vooQuoteAPI).then(res => res.json()).then(data => Number(data.dp.toFixed(2)));
 
     //Arrays for price and percent data
     const priceDataArr = [
@@ -76,14 +76,14 @@ async function getCoreData() {
 
     for (let i = 0; i < 10; i++) {
         priceOutput[i].textContent = priceDataArr[i];
-        
+
         if (percentDataArr[i] > 0) {
             percentOutput[i].textContent = `+${percentDataArr[i]}%`;
             percentOutput[i].style.color = '#50a3ab';
         } else if (percentDataArr[i] == 0) {
             percentOutput[i].textContent = `${percentDataArr[i]}%`;
             percentOutput[i].style.color = '#1d313c';
-        } else {
+        } else if (percentDataArr[i] < 0) {
             percentOutput[i].textContent = `${percentDataArr[i]}%`; //it has a minus sign from response data
             percentOutput[i].style.color = '#c43b39';
         }
@@ -91,5 +91,11 @@ async function getCoreData() {
 }
 
 getCoreData();
+
+// setInterval(getCoreData, 5000);
+
+//удалить проверку на NaN
+//добавить отлов ошибок и туда добавить loading data
+//promise.all или try...catch
 
 
